add06fa0a76718c39b3315fce2b2df9d67ef8ac1
Antonio Acuña <antony_racuba@hotmail.com>
2025-03-18 20:08:12 -0500

    Add: Función para agregar amigos validando que el campo no esté vacío

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo ()
+{
+    const nuevoAmigo = document.getElementById("amigo").value;
+
+    if (nuevoAmigo === "")
+    {
+        alert("Debe ingresar un nombre");
+
+        return;
+    }
+
+    amigos.push(nuevoAmigo);
+
+    document.getElementById("amigo").value = "";
+
+    document.getElementById("amigo").focus();
+}
